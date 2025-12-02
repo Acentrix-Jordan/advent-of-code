4940e647124ac62bedf6f1da99c11cfb8e38a6a4
Acentrix_Jordan <jordan@acentrix.co.uk>
2025-12-02 13:37:19 +0000

    completed: day 1, part 1

--- main.go
+++ main.go
@@ -1,10 +1,61 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	s := "to advent of code 2025"
 	fmt.Printf("Hello and welcome, %s!\n", s)
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	count := 0
+	currentValue := 50
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		movement := scanner.Text()
+		strValue := movement[1:]
+
+		intValue, err := strconv.Atoi(strValue)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if strings.HasPrefix(movement, "L") {
+			currentValue = currentValue - (intValue % 100)
+
+			if currentValue < 0 {
+				currentValue += 100
+			}
+		} else {
+			currentValue = currentValue + (intValue % 100)
+
+			if currentValue > 99 {
+				currentValue -= 100
+			}
+		}
+
+		if currentValue == 0 {
+			count++
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("The safe landed on '0' %d times.", count)
 }
